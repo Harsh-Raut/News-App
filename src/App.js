fa4ea4d6c864809014bbc6732af660688b31f414
Harsh-Raut <rautharsh.00@gmail.com>
2023-08-07 13:14:39 +0530

    NavBars,newsitems and new components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import logo from './logo.svg';
+
 import './App.css';
 
-function App() {
-  return (
-   
-  );
-}
+import React, { Component } from 'react'
+import NavBar from './component/NavBar';
+import News from './component/News';
 
-export default App;
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <NavBar/>
+        <News/>
+        
+      </div>
+    )
+  }
+}
