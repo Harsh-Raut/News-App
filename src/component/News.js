f9088e9a5aae42a4e9454ad617189af6506ca65d
Harsh-Raut <rautharsh.00@gmail.com>
2023-08-07 17:26:45 +0530

    direct endpoint api in CDM method

--- src/component/News.js
+++ src/component/News.js
@@ -2,26 +2,32 @@ import React, { Component } from 'react'
 import NewsItems from './NewsItems'
 
 export class News extends Component {
+   
+  constructor(){
+    super();
+    this.state={
+      article:[],
+      loading:false
+    }
+  }
+  async componentDidMount(){
+    let url="https://newsapi.org/v2/top-headlines?country=in&apiKey=cf6586fdb9a84126a7482184a33f29e5";
+    let data =await fetch(url);
+    let parsedData=await data.json();
+    console.log(parsedData);
+    this.setState({article:parsedData.articles})
+  }
   render() {
     return (
-      <div class="container my-3">
+      <div className="container my-3">
         <h1>NewsMonkey-Headlines</h1>
-        <div className="row">
-            <div className="col md-4 my-3">
-            <NewsItems title="mytitle" description="des"/>
-            </div>
-            <div className="col md-4 my-3">
-            <NewsItems title="mytitle" description="des"/>
-            </div>
-            <div className="col md-4 my-3">
-            <NewsItems title="mytitle" description="des"/>
-            </div>
-        
-        
+        <div className="row" >
+        {this.state.article.map((element)=>{
+            return <div className="col md-4 my-3" key={element.url}>
+            <NewsItems title={element.title} description={element.description} imgUrl={element.urlToImage} newsUrl={element.url}/>
+            </div> })}
+            
         </div>
-        <NewsItems/>
-
-        
       </div>
     )
   }
--- src/component/NewsItems.js
+++ src/component/NewsItems.js
@@ -2,15 +2,15 @@ import React, { Component } from 'react'
 
 export class NewsItems extends Component {
   render() {
-    let {title,description}=this.props;
+    let {title,description,imgUrl,newsUrl}=this.props;
     return (
       <div>
         <div className="card" style={{width: "18rem"}}>
-            <img src="..." className="card-img-top" alt="..."/>
+            <img src={!imgUrl ?"https://www.hindustantimes.com/ht-img/img/2023/08/06/1600x900/PTI08-06-2023-000033B-0_1691320937980_1691320979713.jpg":imgUrl} className="card-img-top" alt="..."/>
             <div className="card-body">
                 <h5 className="card-title">{title}</h5>
                 <p className="card-text"> {description}</p>
-                <a href="#" className="btn btn-primary">Go somewhere</a>
+                <a href={newsUrl} target="_blank"  className="btn btn-sm btn-primary">Read full article</a>
             </div>
         </div>
       </div>
